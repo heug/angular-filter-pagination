17d9bf8b4c74c93ed9cf65165ed46ee04bb02662
Eugene Choe <eugene.choe@gmail.com>
2017-01-23 14:10:18 -0800

    Add page bar

--- public/listController.js
+++ public/listController.js
@@ -3,11 +3,24 @@ angular.module('repo-view.listController', [])
 	.controller('listController', function($scope, repos) {
 		$scope.repoList = [];
 		$scope.pageSize = 10;
-		$scope.currentPage = 0;
-		
+		$scope.currentPage = 1;
+
 		repos.getAll().then(function(res) {
 			$scope.repoList = res.data;
 			console.log($scope.repoList);
 		});
-		
+
+		$scope.numPages = function() {
+			return Math.ceil($scope.repoList.length / $scope.pageSize);
+		};
+
+		$scope.pageRange = function(start, end) {
+			var answer = [];
+			for (var i = start; i <= end; i++) {
+				answer.push(i);
+			}
+			return answer;
+		};
+
+
 	});
--- stubs/data.js
+++ stubs/data.js
@@ -30,6 +30,70 @@ var data = [
     accountName: 'docker',
     repoName: 'thing 5',
     private: false
+  },
+  {
+    id: 1,
+    accountName: 'docker',
+    repoName: 'master',
+    description: 'abcdefghij',
+    private: true
+  },
+  {
+    id: 2,
+    accountName: 'docker',
+    repoName: 'slave',
+    private: true
+  },
+  {
+    id: 3,
+    accountName: 'docker',
+    repoName: 'public',
+    private: false
+  },
+  {
+    id: 4,
+    accountName: 'docker',
+    repoName: 'private',
+    description: 'sdafjaslkfjalskjraser',
+    private: true
+  },
+  {
+    id: 5,
+    accountName: 'docker',
+    repoName: 'thing 5',
+    private: false
+  },
+  {
+    id: 1,
+    accountName: 'docker',
+    repoName: 'master',
+    description: 'abcdefghij',
+    private: true
+  },
+  {
+    id: 2,
+    accountName: 'docker',
+    repoName: 'slave',
+    private: true
+  },
+  {
+    id: 3,
+    accountName: 'docker',
+    repoName: 'public',
+    private: false
+  },
+  {
+    id: 4,
+    accountName: 'docker',
+    repoName: 'private',
+    description: 'sdafjaslkfjalskjraser',
+    private: true
+  },
+  {
+    id: 5,
+    accountName: 'docker',
+    repoName: 'thing 5',
+    private: false
   }
 ];
 
