e5b34e0dd4ca6755bbd0514f99bd65313b314a85
Eugene Choe <eugene.choe@gmail.com>
2017-01-23 21:56:54 -0800

    Continued work

--- public/listController.js
+++ public/listController.js
@@ -2,15 +2,27 @@ angular.module('repo-view.listController', [])
 	
 	.controller('listController', function($scope, repos) {
 		$scope.repoList = [];
+		$scope.showList = [];
 		$scope.scrollBar = [];
 		$scope.pageSize = 10;
 		$scope.currentPage = 1;
-		$scope.pageSet = 1;
 
 		repos.getAll().then(function(res) {
 			$scope.repoList = res.data;
 			$scope.scrollBar = $scope.pageRange();
+			$scope.showList = $scope.showRepos();
+			console.log($scope.showList);
 		});
+
+		$scope.showRepos = function() {
+			if ($scope.repoList.length <= 10) {
+				return $scope.repoList;
+			} else {
+				var end = $scope.currentPage * 10 - 1;
+				var start = end - 10;
+				return $scope.repoList.slice(start, end);
+			}
+		};
 
 		$scope.numPages = function() {
 			return Math.ceil($scope.repoList.length / $scope.pageSize);
